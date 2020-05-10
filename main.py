e2a925066ab93e0c2754814abd040dca740cf59a
Ivan Fedorusov <ifedorusov@gmail.com>
2020-05-11 00:47:09 +0300

    правки

--- main.py
+++ main.py
@@ -105,85 +105,98 @@ def student_test(arr_b, number_x=10):
 def fischer_test():
     dispersion_ad = 0
     f4 = N - d
-    for row in range(len(middle_y)): dispersion_ad += (m * (middle_y[row] - check_result(student_arr, row))) / (N - d)
+    for row in range(len(middle_y)):
+        dispersion_ad += (m * (middle_y[row] - check_result(student_arr, row))) / (N - d)
     f_practice = dispersion_ad / dispersion_b2
     f_theoretical = fischer(f3, f4, q)
     return f_practice < f_theoretical
 
-m = 3
-p = 0.95
-matrix_x = [[] for x_val in range(N)]
-for i in range(len(matrix_x)):
-    if i < 8:
-        x_1 = x1[0] if matrix_pe[i][0] == -1 else x1[1]
-        x_2 = x2[0] if matrix_pe[i][1] == -1 else x2[1]
-        x_3 = x3[0] if matrix_pe[i][2] == -1 else x3[1]
+
+while True:
+    m = 3
+    p = 0.95
+    matrix_x = [[] for x_val in range(N)]
+    for i in range(len(matrix_x)):
+        if i < 8:
+            x_1 = x1[0] if matrix_pe[i][0] == -1 else x1[1]
+            x_2 = x2[0] if matrix_pe[i][1] == -1 else x2[1]
+            x_3 = x3[0] if matrix_pe[i][2] == -1 else x3[1]
+        else:
+            arr_x = x(matrix_pe[i][0], matrix_pe[i][1], matrix_pe[i][2])
+            x_1, x_2, x_3 = arr_x
+        matrix_x[i] = [x_1, x_2, x_3, x_1 * x_2, x_1 * x_3, x_2 * x_3, x_1 * x_2 * x_3, x_1 ** 2, x_2 ** 2, x_3 ** 2]
+
+    homogeneity, dispersion_y = False, None
+    matrix_y = generate_matrix()
+    middle_x = average_value(matrix_x, 0)
+    middle_y = average_value(matrix_y, 1)
+    matrix = [(matrix_x[i] + matrix_y[i]) for i in range(N)]
+    mx_i = middle_x
+    my = sum(middle_y) / 15
+
+    values = [
+        [1, mx_i[0], mx_i[1], mx_i[2], mx_i[3], mx_i[4], mx_i[5], mx_i[6], mx_i[7], mx_i[8], mx_i[9]],
+        [mx_i[0], a(1, 1), a(1, 2), a(1, 3), a(1, 4), a(1, 5), a(1, 6), a(1, 7), a(1, 8), a(1, 9), a(1, 10)],
+        [mx_i[1], a(2, 1), a(2, 2), a(2, 3), a(2, 4), a(2, 5), a(2, 6), a(2, 7), a(2, 8), a(2, 9), a(2, 10)],
+        [mx_i[2], a(3, 1), a(3, 2), a(3, 3), a(3, 4), a(3, 5), a(3, 6), a(3, 7), a(3, 8), a(3, 9), a(3, 10)],
+        [mx_i[3], a(4, 1), a(4, 2), a(4, 3), a(4, 4), a(4, 5), a(4, 6), a(4, 7), a(4, 8), a(4, 9), a(4, 10)],
+        [mx_i[4], a(5, 1), a(5, 2), a(5, 3), a(5, 4), a(5, 5), a(5, 6), a(5, 7), a(5, 8), a(5, 9), a(5, 10)],
+        [mx_i[5], a(6, 1), a(6, 2), a(6, 3), a(6, 4), a(6, 5), a(6, 6), a(6, 7), a(6, 8), a(6, 9), a(6, 10)],
+        [mx_i[6], a(7, 1), a(7, 2), a(7, 3), a(7, 4), a(7, 5), a(7, 6), a(7, 7), a(7, 8), a(7, 9), a(7, 10)],
+        [mx_i[7], a(8, 1), a(8, 2), a(8, 3), a(8, 4), a(8, 5), a(8, 6), a(8, 7), a(8, 8), a(8, 9), a(8, 10)],
+        [mx_i[8], a(9, 1), a(9, 2), a(9, 3), a(9, 4), a(9, 5), a(9, 6), a(9, 7), a(9, 8), a(9, 9), a(9, 10)],
+        [mx_i[9], a(10, 1), a(10, 2), a(10, 3), a(10, 4), a(10, 5), a(10, 6), a(10, 7), a(10, 8), a(10, 9), a(10, 10)]
+    ]
+    known_values = [my, find_known(1), find_known(2), find_known(3), find_known(4), find_known(5), find_known(6), find_known(7), find_known(8), find_known(9), find_known(10)]
+
+    beta = solve(values, known_values)
+    print("\nРівняння регресії:")
+    print("{:.3f} + {:.3f} * X1 + {:.3f} * X2 + {:.3f} * X3 + {:.3f} * Х1X2 + {:.3f} * Х1X3 + {:.3f} * Х2X3 + {:.3f} * Х1Х2X3 + {:.3f} * X11^2 + {:.3f} * X22^2 + {:.3f} * X33^2 = ŷ\n\nПеревірка".format(beta[0], beta[1], beta[2], beta[3], beta[4], beta[5], beta[6], beta[7], beta[8], beta[9], beta[10]))
+    for i in range(N): print("ŷ{} = {:.3f} ≈ {:.3f}".format((i + 1), check_result(beta, i), middle_y[i]))
+
+    while not homogeneity:
+        print("\n" + " " * 65 + "Матриця планування" + " " * 65 + "      X1           X2           X3          X1X2        X1X3         X2X3         X1X2X3       X1X1         X2X2         X3X3          Yi ...")
+        for row in range(N):
+            print(" ", end=' ')
+            for column in range(len(matrix[0])): print("{:^12.3f}".format(matrix[row][column]), end=' ')
+            print(" ")
+        print("\n")
+        dispersion_y = [0.0 for x in range(N)]
+        for i in range(N):
+            dispersion_i = 0
+            for j in range(m): dispersion_i += (matrix_y[i][j] - middle_y[i]) ** 2
+            dispersion_y.append(dispersion_i / (m - 1))
+        f1 = m - 1
+        f2 = N
+        f3 = f1 * f2
+        q = 1 - p
+        Gp = max(dispersion_y) / sum(dispersion_y)
+        print("Критерій Кохрена")
+        Gt = cochrane(f2, f1, q)
+        if Gt > Gp and m < 25:
+            print("Дисперсія однорідна з q = {:.2f}!\n".format(q))
+            homogeneity = True
+        else:
+            print("Дисперсія не є однорідною. Повторна спроба при m += 1...")
+            m += 1
+        if m == 25: exit()
+
+
+    dispersion_b2 = sum(dispersion_y) / (N * N * m)
+    student_arr = list(student_test(beta))
+    print("Рівняння регресії за критерієм Стьюдента\n{:.3f} + {:.3f} * X1 + {:.3f} * X2 + {:.3f} * X3 + {:.3f} * Х1X2 + {:.3f} * Х1X3 + {:.3f} * Х2X3 + {:.3f} * Х1Х2X3 + {:.3f} * X11^2 + {:.3f} * X22^2 + {:.3f} * X33^2 = ŷ\n\nПеревірка".format(student_arr[0], student_arr[1], student_arr[2], student_arr[3], student_arr[4], student_arr[5], student_arr[6], student_arr[7], student_arr[8], student_arr[9], student_arr[10]))
+    for i in range(N): print("ŷ{} = {:.3f} ≈ {:.3f}".format((i + 1), check_result(student_arr, i), middle_y[i]))
+    d = 11 - student_arr.count(0)
+    dependent += N - d
+    independent += d
+    if fischer_test():
+        print('Математична модель адекватна експериментальним даним.')
+        break
     else:
-        arr_x = x(matrix_pe[i][0], matrix_pe[i][1], matrix_pe[i][2])
-        x_1, x_2, x_3 = arr_x
-    matrix_x[i] = [x_1, x_2, x_3, x_1 * x_2, x_1 * x_3, x_2 * x_3, x_1 * x_2 * x_3, x_1 ** 2, x_2 ** 2, x_3 ** 2]
-
-homogeneity, dispersion_y = False, None
-matrix_y = generate_matrix()
-middle_x = average_value(matrix_x, 0)
-middle_y = average_value(matrix_y, 1)
-matrix = [(matrix_x[i] + matrix_y[i]) for i in range(N)]
-mx_i = middle_x
-my = sum(middle_y) / 15
-
-values = [
-    [1, mx_i[0], mx_i[1], mx_i[2], mx_i[3], mx_i[4], mx_i[5], mx_i[6], mx_i[7], mx_i[8], mx_i[9]],
-    [mx_i[0], a(1, 1), a(1, 2), a(1, 3), a(1, 4), a(1, 5), a(1, 6), a(1, 7), a(1, 8), a(1, 9), a(1, 10)],
-    [mx_i[1], a(2, 1), a(2, 2), a(2, 3), a(2, 4), a(2, 5), a(2, 6), a(2, 7), a(2, 8), a(2, 9), a(2, 10)],
-    [mx_i[2], a(3, 1), a(3, 2), a(3, 3), a(3, 4), a(3, 5), a(3, 6), a(3, 7), a(3, 8), a(3, 9), a(3, 10)],
-    [mx_i[3], a(4, 1), a(4, 2), a(4, 3), a(4, 4), a(4, 5), a(4, 6), a(4, 7), a(4, 8), a(4, 9), a(4, 10)],
-    [mx_i[4], a(5, 1), a(5, 2), a(5, 3), a(5, 4), a(5, 5), a(5, 6), a(5, 7), a(5, 8), a(5, 9), a(5, 10)],
-    [mx_i[5], a(6, 1), a(6, 2), a(6, 3), a(6, 4), a(6, 5), a(6, 6), a(6, 7), a(6, 8), a(6, 9), a(6, 10)],
-    [mx_i[6], a(7, 1), a(7, 2), a(7, 3), a(7, 4), a(7, 5), a(7, 6), a(7, 7), a(7, 8), a(7, 9), a(7, 10)],
-    [mx_i[7], a(8, 1), a(8, 2), a(8, 3), a(8, 4), a(8, 5), a(8, 6), a(8, 7), a(8, 8), a(8, 9), a(8, 10)],
-    [mx_i[8], a(9, 1), a(9, 2), a(9, 3), a(9, 4), a(9, 5), a(9, 6), a(9, 7), a(9, 8), a(9, 9), a(9, 10)],
-    [mx_i[9], a(10, 1), a(10, 2), a(10, 3), a(10, 4), a(10, 5), a(10, 6), a(10, 7), a(10, 8), a(10, 9), a(10, 10)]
-]
-known_values = [my, find_known(1), find_known(2), find_known(3), find_known(4), find_known(5), find_known(6), find_known(7), find_known(8), find_known(9), find_known(10)]
-
-beta = solve(values, known_values)
-print("\nРівняння регресії:")
-print("{:.3f} + {:.3f} * X1 + {:.3f} * X2 + {:.3f} * X3 + {:.3f} * Х1X2 + {:.3f} * Х1X3 + {:.3f} * Х2X3 + {:.3f} * Х1Х2X3 + {:.3f} * X11^2 + {:.3f} * X22^2 + {:.3f} * X33^2 = ŷ\n\nПеревірка".format(beta[0], beta[1], beta[2], beta[3], beta[4], beta[5], beta[6], beta[7], beta[8], beta[9], beta[10]))
-for i in range(N): print("ŷ{} = {:.3f} ≈ {:.3f}".format((i + 1), check_result(beta, i), middle_y[i]))
-
-while not homogeneity:
-    print("\n" + " " * 65 + "Матриця планування" + " " * 65 + "      X1           X2           X3          X1X2        X1X3         X2X3         X1X2X3       X1X1         X2X2         X3X3          Yi ...")
-    for row in range(N):
-        print(" ", end=' ')
-        for column in range(len(matrix[0])): print("{:^12.3f}".format(matrix[row][column]), end=' ')
-        print(" ")
-    print("\n")
-    dispersion_y = [0.0 for x in range(N)]
-    for i in range(N):
-        dispersion_i = 0
-        for j in range(m): dispersion_i += (matrix_y[i][j] - middle_y[i]) ** 2
-        dispersion_y.append(dispersion_i / (m - 1))
-    f1 = m - 1
-    f2 = N
-    f3 = f1 * f2
-    q = 1 - p
-    Gp = max(dispersion_y) / sum(dispersion_y)
-    print("Критерій Кохрена")
-    Gt = cochrane(f2, f1, q)
-    if Gt > Gp and m < 25:
-        print("Дисперсія однорідна з q = {:.2f}!\n".format(q))
-        homogeneity = True
-    else:
-        print("Дисперсія не є однорідною. Повторна спроба при m += 1...")
-        m += 1
-    if m == 25: exit()
-
-
-dispersion_b2 = sum(dispersion_y) / (N * N * m)
-student_arr = list(student_test(beta))
-print("Рівняння регресії за критерієм Стьюдента\n{:.3f} + {:.3f} * X1 + {:.3f} * X2 + {:.3f} * X3 + {:.3f} * Х1X2 + {:.3f} * Х1X3 + {:.3f} * Х2X3 + {:.3f} * Х1Х2X3 + {:.3f} * X11^2 + {:.3f} * X22^2 + {:.3f} * X33^2 = ŷ\n\nПеревірка".format(student_arr[0], student_arr[1], student_arr[2], student_arr[3], student_arr[4], student_arr[5], student_arr[6], student_arr[7], student_arr[8], student_arr[9], student_arr[10]))
-for i in range(N): print("ŷ{} = {:.3f} ≈ {:.3f}".format((i + 1), check_result(student_arr, i), middle_y[i]))
-d = 11 - student_arr.count(0)
-dependent += N - d
-independent += d
-print('Математична модель адекватна експериментальним даним.') if fischer_test() else print('Математична модель не є адекватною експериментальним даним.')
+        print('Математична модель не є адекватною експериментальним даним.')
+        x1[0] *= 1.7
+        x1[1] *= 1.7
+        x2[0] *= 1.7
+        x2[1] *= 1.7
+        x3[0] *= 1.7
+        x3[1] *= 1.7
